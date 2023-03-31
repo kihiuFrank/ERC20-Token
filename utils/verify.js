185a9052f08f86fa98b4138912649b63748db4b4
kihiuFrank <franklinekihiu@gmail.com>
2023-03-31 15:38:16 +0300

    contract verification

---
+++ utils/verify.js
@@ -0,0 +1,19 @@
+import { run } from "hardhat"
+
+const verify = async (contractAddress, args) => {
+    console.log("Verifying Contract........")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (error) {
+        if (error.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!")
+        } else {
+            console.log(error)
+        }
+    }
+}
+
+export default { verify }
